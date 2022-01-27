27e2b8bb87aa9d1c913818977f6040955bfc0771
mthous72 <mthous01@hotmail.com>
2022-01-27 12:01:18 -0500

    dre9: initial conversion
    Change-Id: Ic229431fd18fc15f5fdcfee8bb7ea81c04c85907

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lemonade.mk \
-    $(LOCAL_DIR)/ev_lemonade.mk
+    $(LOCAL_DIR)/aosp_dre9.mk \
+    $(LOCAL_DIR)/ev_dre9.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lemonade-user \
-    aosp_lemonade-userdebug \
-    aosp_lemonade-eng \
-    ev_lemonade-user \
-    ev_lemonade-userdebug \
-    ev_lemonade-eng
+    aosp_dre9-user \
+    aosp_dre9-userdebug \
+    aosp_dre9-eng \
+    ev_dre9-user \
+    ev_dre9-userdebug \
+    ev_dre9-eng
--- ev_dre9.mk
+++ ev_dre9.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/aosp_lemonade.mk)
+# Inherit from dre9 device
+$(call inherit-product, device/oneplus/dre9/aosp_dre9.mk)
 
 # Boot animation
 BOOT_ANIMATION_SIZE := 1080p
@@ -13,17 +13,18 @@ BOOT_ANIMATION_SIZE := 1080p
 # Inherit some common Evervolv stuff.
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 
-PRODUCT_NAME := ev_lemonade
+PRODUCT_NAME := ev_dre9
 PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := DE2117
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
+PRODUCT_SYSTEM_NAME := OnePlusN200
+PRODUCT_SYSTEM_DEVICE := OnePlusN200
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 12 RKQ1.211119.001 R.202208121853 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlusN200-user 11   release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:12/RKQ1.211119.001/R.202208121853:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlusN200/OnePlusN200:11/RKQ1.201217.002/2107241556:user/release-keys
